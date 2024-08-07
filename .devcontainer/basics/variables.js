a75a09d20b85c9390651ca0bd2281b848cf04280
PriyankRana <123861930+PriyankRana@users.noreply.github.com>
2024-08-07 05:19:44 +0000

    variables

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 517751
+let accountEmail = "pprajput@5177.com"
+var accountPassword = "12345"
+accountCity = "Dehradun"
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "54321"
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
