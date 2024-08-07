6b93aadc8d76e5e376884fb1c044cf837be147e5
PriyankRana <123861930+PriyankRana@users.noreply.github.com>
2024-08-07 16:38:16 +0000

    memory

---
+++ .devcontainer/basics/memory.js
@@ -0,0 +1,16 @@
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myname = "Priyank"
+
+let anothername = myname
+anothername = "Harsh"
+
+console.log(anothername);
+console.log(myname);
+
+let user = {
+    email: "user$google.com",
+    upi: "user$ybl"
+}
+
+console.log(user);
