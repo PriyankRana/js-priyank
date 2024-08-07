b56f75e7f71f8bb07121d79deb55b2d422b5bbd4
PriyankRana <123861930+PriyankRana@users.noreply.github.com>
2024-08-07 05:20:02 +0000

    datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //Treat all js code as newer version
+
+//alert(5 + 6) // we are using nodejs, not browser
+
+let name = "Priyank"
+let age = 23
+let isLoggedIn = false
+let state;
+
+
+//number => to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+//undefined => 
+//symbol => unique
+
+// object
+
+console.log(typeof isLoggedIn);
+console.log(typeof null); //object
+console.log(typeof undefined);
+
