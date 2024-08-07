c4d1d04120d598c640180e25bd24b07926446661
PriyankRana <123861930+PriyankRana@users.noreply.github.com>
2024-08-07 05:46:53 +0000

    conversion datatype

---
+++ .devcontainer/basics/conversionoperation.js
@@ -0,0 +1,16 @@
+let score = "51"
+
+// console.log( typeof score);
+// console.log(typeof(score));
+
+let valueInnumber = Number(score)
+// console.log( typeof valueInnumber);
+
+let IsLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 51
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
